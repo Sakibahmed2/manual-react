60b00a14d94a67b41658f603cfb5599d2ba7e238
unknown <mauijaloskor@gmail.com>
2024-01-06 14:09:35 +0600

    setup rollup

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-// const helloWorld = () => {
-//   return React.createElement("p", {}, "Hello world");
+import { helloWorld } from "./another";
+
+const App = () => {
+  {
+    return React.createElement(
+      "h1",
+      { style: { color: "red" } },
+      "This is a fokira react app",
+      helloWorld()
+    );
+  }
+};
+
+// const HelloWorld = () => {
+//   return <h1>Hello world</h1>;
 // };
 
 // const App = () => {
-//   {
-//     return React.createElement(
-//       "h1",
-//       { style: { color: "red" } },
-//       "This is a fokira react app",
-//       helloWorld()
-//     );
-//   }
+//   return (
+//     <React.Fragment>
+//       <div>This is a fokira react app</div>
+//       <HelloWorld></HelloWorld>
+//     </React.Fragment>
+//   );
 // };
-
-const HelloWorld = () => {
-  return <h1>Hello world</h1>;
-};
-
-const App = () => {
-  return (
-    <React.Fragment>
-      <div>This is a fokira react app</div>
-      <HelloWorld></HelloWorld>
-    </React.Fragment>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(React.createElement(App));
